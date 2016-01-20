09b51b1d2fc20d0d3695ee32facdc510d96ec3b5
Robin Elden <me@robin.io>
2016-01-20 13:48:29 +1100

    antics

--- cutiebot.rb
+++ cutiebot.rb
@@ -5,6 +5,38 @@ class CutieBot < SlackRubyBot::Bot
     client.say(text: 'pong', channel: data.channel)
   end
 
+  command 'exit' do |client, data, match|
+    abort
+  end
+
+#  match /(?<name>[a-zA-Z]*[e][n][a-zA-Z]*)/ do |client, data, match|
+#    name = []
+#    match[:name].split("en").each do |c|
+#     name.push(c)
+#    end
+#    client.say(channel:data.channel, text: name.join(""))
+#  end
+
+match /(?<word>\w*[e][n]\w*)/ do |client, data, match|
+    word = []
+    match[:word].split("en").each do |c|
+        word.push(c)
+    end	
+    client.say(channel:data.channel, text: word.join("erin"))
+end
+  match /\ in\w+/i do |client, data, match|
+    client.say(text: data.text.gsub(' in', ' erin'), channel: data.channel)
+  end
+
+  match /tran/ do |client, data, match|
+    client.say(text: data.text.gsub('tran' , 'tram'), channel: data.channel)
+  end
+
+
+  match /tram/ do |client, data, match|
+    client.say(text: data.text.gsub('tram' , 'tran'), channel: data.channel)
+  end
+
   match /thanks (?<name>\w*)/i do |client, data, match|
     name = []
     done = 0
